10876b1b9992948eced71623b1df52f1948f09cf
Baron C <20404222+baron15cao@users.noreply.github.com>
2022-12-01 23:31:23 -0600

    Removed a few strings to make my one-line shorter.

--- day1Better.rb
+++ day1Better.rb
@@ -1,3 +1,3 @@
-puts ("The sum of top 3 elves carrying the most calories is #{IO.readlines("input.txt", chomp: true).slice_when { |a,b| (a.empty?) ^ (b.empty?) }.to_a.map{ |inner_array| inner_array.map(&:to_i).inject(&:+) }.reject(&:nil?).sort!.reverse.first(3).inject(0, :+)}.") 
+puts ("The sum of top 3 elves carrying the most calories is #{IO.readlines("input.txt", chomp: true).slice_when { |a,b| (a.empty?) ^ (b.empty?) }.to_a.map{ |n| n.map(&:to_i).sum }.reject(&:nil?).sort.last(3).sum}.") 
 
 #Did it in one line
