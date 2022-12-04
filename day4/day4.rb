d598407f63aa915359b3807bf88f43101c6a6517
Baron C <20404222+baron15cao@users.noreply.github.com>
2022-12-04 00:12:42 -0600

    Added a day4 solution using just four lines

--- day4/day4.rb
+++ day4/day4.rb
@@ -7,6 +7,7 @@ File.open("input.txt", "r").each do |line|
     elf2e = line.partition(',')[2][line.partition(',')[2].index('-')+1..-1].to_i
     puts "Elf1 starts at #{elf1s} and ends at #{elf1e}."
     puts "Elf2 starts at #{elf2s} and ends at #{elf2e}."
+
     
     if ((elf1s <= elf2s && elf1e >= elf2e) || (elf2s <= elf1s && elf2e >= elf1e))
         count1 += 1
@@ -21,3 +22,8 @@ end
 
 puts "Part 1 equals #{count1}."
 puts "Part 2 equals #{count2}."
+
+
+a = [ "a", "b", "c" ]
+t = ["a", "b"]
+puts "Test: #{a.include?(t) }" 
