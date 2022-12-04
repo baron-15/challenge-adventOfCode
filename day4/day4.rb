84468ca706f9f939bd9e0d5c82c9c3b855762390
Baron C <20404222+baron15cao@users.noreply.github.com>
2022-12-03 23:30:46 -0600

    Finished Advent of Code Day 4

---
+++ day4/day4.rb
@@ -0,0 +1,23 @@
+count1 = 0
+count2 = 0
+File.open("input.txt", "r").each do |line|
+    elf1s = line.partition(',')[0][0..line.partition(',')[0].index('-')].to_i
+    elf1e = line.partition(',')[0][line.partition(',')[0].index('-')+1..-1].to_i
+    elf2s = line.partition(',')[2][0..line.partition(',')[2].index('-')].to_i
+    elf2e = line.partition(',')[2][line.partition(',')[2].index('-')+1..-1].to_i
+    puts "Elf1 starts at #{elf1s} and ends at #{elf1e}."
+    puts "Elf2 starts at #{elf2s} and ends at #{elf2e}."
+    
+    if ((elf1s <= elf2s && elf1e >= elf2e) || (elf2s <= elf1s && elf2e >= elf1e))
+        count1 += 1
+        puts "Overlap part 1!"
+    end
+
+    if ((elf1e >= elf2s && elf1s <= elf2e) || (elf2e >= elf1s && elf2s <= elf1e))
+        count2 += 1
+        puts "Overlap part 2!"
+    end
+end
+
+puts "Part 1 equals #{count1}."
+puts "Part 2 equals #{count2}."
